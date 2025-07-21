6b14dbdeae033c65adc6e910067867979e9bda5e
Lmeadows16 <leyton.meadows@marquette.edu>
2025-07-21 15:48:10 -0500

    Added configure.js to public

---
+++ Edgebrook Ace/Orders/public/configure.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDNPziZqj3zHL_y01fXXH5uBYQB0ctS6T0',
+    authDomain: 'ace-orders-9de41.firebaseapp.com',
+    projectId: 'ace-orders-9de41',
+    storageBucket: 'ace-orders-9de41.firebasestorage.app',
+    messagingSenderId: '931385542602',
+    appId: '1:931385542602:web:9858f492ad65e36ebb7133'
+}
